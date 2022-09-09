e628d6a879308d0a8501dda07df5b67b4ec7a496
JerrisonChang <jerrisonChang@gmail.com>
2022-09-09 09:35:15 -0400

    move things around

--- execution_scripts/pipedlined_testing.ps1
+++ execution_scripts/pipedlined_testing.ps1
@@ -1,5 +1,5 @@
-$TAG = "NGSS2C_ED_mod"
-$input_text_file = "DATA\test_NGSS2C_ED_modified.txt"
+$TAG = "NGSS2C_rest"
+$input_text_file = "DATA\ngss\sentences_ess2.C_rest.txt"
 $targetid_model = "targetid0724"
 $frameid_model = "frameid0724"
 $argid_model = "argid0725"
@@ -9,8 +9,8 @@ conda activate open-sesame
 # python -m sesame.targetid --mode predict  --model_name $targetid_model `
 #     --raw_input $input_text_file  --output_suffix $TAG
 
-# python -m sesame.frameid --mode predict  --model_name $frameid_model `
-#     --raw_input logs\$targetid_model\predicted-frames__$TAG.conll --output_suffix $TAG
+python -m sesame.frameid --mode predict  --model_name $frameid_model `
+    --raw_input logs\$targetid_model\predicted-targets__$TAG.conll --output_suffix $TAG
 
 python -m sesame.argid --mode predict --model_name $argid_model `
     --raw_input logs\$frameid_model\predicted-frames__$TAG.conll --output_suffix $TAG
