c48a0e5a1bfd7574d08b25b05a76bd8cea32cb5a
JerrisonChang <jerrisonChang@gmail.com>
2023-03-12 21:23:28 -0400

    try to enable running multiple files at once.

--- execution_scripts/pipedlined_testing.ps1
+++ execution_scripts/pipedlined_testing.ps1
@@ -1,12 +1,17 @@
 # $GRADE = "2s"
-$TAG = "student_276"
 # $input_text_file = "DATA\ngss\PS2.A_$($GRADE).txt"
-$input_text_file = "DATA\student_notes__22.txt"
-$targetid_model = "targetid0724"
-$frameid_model = "frameid0724"
-$argid_model = "argid0725"
-
+# $note_id = "345"
+# $input_text_file = "DATA\student_notes__$($note_id).txt"
+# $TAG = "student_$($note_id)"
 conda activate open-sesame
+
+$grade2tag = @{
+    '2s' = ""
+    '5s' = ""
+    '8s' = ""
+    '12s' = ""
+}
+
 
 function Exit-With-Error {
     param ($output)
@@ -17,23 +22,39 @@ function Exit-With-Error {
     exit
 }
 
-$target_output = python -m sesame.targetid --mode predict  --model_name $targetid_model `
-    --raw_input $input_text_file  --output_suffix $TAG
-if ($LASTEXITCODE -ne 0 ) { Exit-With-Error -output $target_output }
-[console]::beep(1000,200)
+function Start-Single-File {
+    param (
+        [string]$input_file, 
+        [string]$tag,
+        [string]$tm = "targetid0724", 
+        [string]$fm = "frameid0724", 
+        [string]$am = "argid0725"
+    )
 
-$frame_output = python -m sesame.frameid --mode predict  --model_name $frameid_model `
-    --raw_input logs\$targetid_model\predicted-targets__$TAG.conll --output_suffix "$($TAG)_singleframe"
-if ($LASTEXITCODE -ne 0 ) { Exit-With-Error -output $frame_output }
-[console]::beep(1000,200)
+    $target_output = python -m sesame.targetid --mode predict  --model_name $tm `
+        --raw_input $input_file  --output_suffix $tag
+    if ($LASTEXITCODE -ne 0 ) { Exit-With-Error -output $target_output }
+    [console]::beep(1000,200)
+    
+    $frame_output = python -m sesame.frameid --mode predict  --model_name $fm `
+        --raw_input logs\$tm\predicted-targets__$tag.conll --output_suffix "$($tag)_singleframe"
+    if ($LASTEXITCODE -ne 0 ) { Exit-With-Error -output $frame_output }
+    [console]::beep(1000,200)
+    
+    $fe_output = python -m sesame.argid --mode predict --model_name $am `
+        --raw_input logs\$fm\predicted-frames__${tag}_singleframe.conll --output_suffix $tag
+    if ($LASTEXITCODE -ne 0 ) { Exit-With-Error -output $fe_output }
+    
+    
+    [console]::beep(2000,800)
+    Start-Sleep -m 100
+    [console]::beep(2000,800)
+    Start-Sleep -m 100
+    [console]::beep(2000,800)
+}
 
-$fe_output = python -m sesame.argid --mode predict --model_name $argid_model `
-    --raw_input logs\$frameid_model\predicted-frames__${TAG}_singleframe.conll --output_suffix $TAG
-if ($LASTEXITCODE -ne 0 ) { Exit-With-Error -output $fe_output }
 
-
-[console]::beep(2000,200)
-Start-Sleep -m 100
-[console]::beep(2000,800)
-Start-Sleep -m 100
-[console]::beep(2000,800)
+foreach ($h in $grade2tag.GetEnumerator()) {
+    $file_path = "DATA\ngss\LS1.C_$($h.Name).txt"
+    Start-Single-File -input_file $file_path -tag "0312"
+}
